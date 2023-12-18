36a31c940e97cec65399abe8a28077ab3c9df0e5
Gulam Kibria Chowdhury <gkchowdhury101@gmail.com>
2023-12-19 00:25:11 +0600

    Time: 690 ms (76.98%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,1 +1,5 @@
-select DISTINCT author_id as id from Views where author_id=viewer_id order by id;
+SELECT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+ORDER BY author_id;
