5102e2c13483c55d9cefadd5e051c6d190c50bbd
Gulam Kibria Chowdhury <gkchowdhury101@gmail.com>
2023-12-26 23:35:29 +0600

    Time: 1254 ms (51.48%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -13,17 +13,17 @@
 # GROUP BY s.user_id; #AC
 
 
-# select s.user_id, round(avg(if(c.action = "confirmed",1,0)),2) as confirmation_rate 
-# from Signups as s left join Confirmations as c on s.user_id = c.user_id
-# group by user_id #AC
+select s.user_id, round(avg(if(c.action = "confirmed",1,0)),2) as confirmation_rate 
+from Signups as s left join Confirmations as c on s.user_id = c.user_id
+group by user_id #AC
 
-SELECT a.user_id,
-   round(COALESCE ((
-        (SELECT COUNT(*) FROM Confirmations WHERE action='confirmed' AND user_id = a.user_id) /
-        COALESCE((COUNT(b.user_id) ), 1)
-    ),0),2) AS confirmation_rate
-FROM Signups a
-LEFT JOIN Confirmations b ON a.user_id = b.user_id
-GROUP BY a.user_id; #AC
+# SELECT a.user_id,
+#    round(COALESCE ((
+#         (SELECT COUNT(*) FROM Confirmations WHERE action='confirmed' AND user_id = a.user_id) /
+#         COALESCE((COUNT(b.user_id) ), 1)
+#     ),0),2) AS confirmation_rate
+# FROM Signups a
+# LEFT JOIN Confirmations b ON a.user_id = b.user_id
+# GROUP BY a.user_id; #AC
 
 
