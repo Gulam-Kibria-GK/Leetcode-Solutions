9dda29e73b9485fb934c1f281683c1d7ddce6be6
Gulam Kibria Chowdhury <gkchowdhury101@gmail.com>
2024-01-04 01:08:45 +0600

    Time: 1485 ms (8.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+select p.project_id , ifnull(round(sum(e.experience_years)/count(p.project_id),2),0) as average_years from Project p left join Employee e on p.employee_id = e.employee_id where experience_years is not null
+group by p.project_id
