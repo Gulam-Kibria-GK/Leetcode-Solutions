d0f09ca608b8952aa2478ff12939d42888291d89
Gulam Kibria Chowdhury <gkchowdhury101@gmail.com>
2024-01-03 11:59:14 +0600

    Time: 1369 ms (54.46%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,4 @@
-SELECT p.product_id, COALESCE(ROUND(sum(u.units * p.price) / NULLIF(sum(u.units), 0), 2), 0) as average_price
+SELECT p.product_id, COALESCE(ROUND(sum(u.units * p.price) /(sum(u.units)), 2), 0) as average_price
 FROM Prices p
 left JOIN UnitsSold u ON p.product_id = u.product_id
 and u.purchase_date BETWEEN p.start_date AND p.end_date
