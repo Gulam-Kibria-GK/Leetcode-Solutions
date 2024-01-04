f66f3f91946f6e0a1a3b75750a587f270efb28ba
Gulam Kibria Chowdhury <gkchowdhury101@gmail.com>
2024-01-04 15:22:45 +0600

    Time: 1032 ms (7.73%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,3 +3,14 @@
 select query_name, round((sum(rating/position)/count(query_name)),2) as quality , round(sum(if(rating<3 , 1,0))/count(query_name)*100,2) as poor_query_percentage from Queries 
 where query_name is not null
 group by query_name 
+
+
+# SELECT 
+#     query_name,
+#     ROUND(AVG(rating/position), 2) AS quality,
+#     ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage
+# FROM 
+#     Queries
+# WHERE query_name is not null
+# GROUP BY
+#     query_name;
